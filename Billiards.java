ba7851dd0c1249654efe89cc0de89ee52b3254a1
ysi0000 <ysi0000@alu.ubu.es>
2018-03-13 23:09:47 +0100

    Init balls y añadida foto a la carpeta
    Se ha hecho el metodo init

--- Billiards.java
+++ Billiards.java
@@ -19,8 +19,8 @@ public class Billiards extends JFrame {
 	private Board board;
 
 	// TODO update with number of group label. See practice statement.
-	private final int N_BALL = 2;
-	private Ball[] balls;
+	private final int N_BALL = 10;
+	private Ball[] balls = new Ball[N_BALL];
 
 	public Billiards() {
 
@@ -54,6 +54,9 @@ public class Billiards extends JFrame {
 
 	private void initBalls() {
 		// TODO init balls
+		for (int i = 0; i < N_BALL; i++) {
+			balls[i] = new Ball();
+		}
 	}
 
 	private class StartListener implements ActionListener {
