e91e282ef2d3d7e822e6f73c3bb66f2bc1f0e1cf
gca0011 <gca0011@alu.ubu.es>
2018-03-13 23:25:32 +0100

    // TODO Code is executed when stop button is pushed
    Realizamos la funcion StopListener

--- Billiards.java
+++ Billiards.java
@@ -76,7 +76,9 @@ public class Billiards extends JFrame {
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			// TODO Code is executed when stop button is pushed
-
+			for (int i = 0; i < N_BALL; i++) {
+				hilo[i].interrupt();
+			}
 		}
 	}
 
